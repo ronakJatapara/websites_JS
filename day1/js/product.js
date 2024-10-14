8133f1f20372a11148277b437a08233c3bf9f00e
JAVASCRIPT2 <ronakjatapara76@gmail.com>
2024-10-14 21:05:13 +0530

    added

---
+++ day1/js/product.js
@@ -0,0 +1,46 @@
+
+
+
+fetch(`http://localhost:3000/products`)
+.then((res)=>{
+    return res.json();
+})
+.then((res)=>{
+   document.querySelector(".box").innerHTML=view(res)
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
+
+function view(arr)
+{
+    return arr.map((el,ind)=>{
+        return`
+        <div class="col-3 border d-flex justify-content-center align-items-center flex-column mt-5" id="box2">
+     <div class="img2">
+     <img src="${el.img}">
+     </div>
+     <div class="text2 text-center">
+     <p>${el.title}</p>
+     <p>${el.price}</p>
+ 
+</div>
+    <div class="row border" id="box3">
+  <div class="col-3" id="last1">
+  <i class="fa-regular fa-heart"></i>
+  </div>
+  <div class="col-9" id="last2">
+  Add To Bag
+  </div>
+
+     </div>
+        
+
+        
+
+        </div>
+        `
+    }).join(" ")
+}
